4023aa31c6f0e8b3a8d2c4141a42d14758707764
Thomas Manos Bajis <thomas.bajis@gmail.com>
2020-06-26 10:44:39 -0400

    Initial Commit

---
+++ NetworkingPrototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkingPrototype
+//
+//  Created by Thomas Manos Bajis on 6/26/20.
+//  Copyright © 2020 Thomas Manos Bajis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
